08f92fff4b2801139d93bf8573d624fcccb2ad56
AhmedShaiban <143512500+AhmedShaiban@users.noreply.github.com>
2023-11-09 18:32:40 +0100

    For_Loop

---
+++ main.js
@@ -0,0 +1,73 @@
+// Opdracht 1
+
+for (var i = 0; i <= 9; i++) {
+    console.log(i);
+  }
+  console.log("Einde programma");
+
+ // Opdracht 2
+
+for (var i = 0; i <= 20; i++) {
+    console.log(i);
+  }
+  console.log("Einde programma");
+
+ // Opdracht 3
+
+ for (var i = 2; i <= 20; i++) {
+    console.log(i);
+  }
+  console.log("Einde programma");
+
+  // Opdracht 4
+
+for (var i = 10; i >= 0; i--) {
+    console.log(i);
+  }
+  console.log("Einde programma");
+
+   // Opdracht 5
+
+for (var i = 40; i >= 4; i--) {
+    console.log(i);
+  }
+  console.log("Einde programma");
+
+  
+   // Opdracht 6
+
+for (var i = 0; i < 8; i++) {
+    console.log("Ik ontwikkel apps");
+  }
+
+     // Opdracht 7
+
+for (var i = 0; i <= 12; i++) {
+    console.log("Dit is loop: " + i);
+  }
+
+    
+ // Opdracht 8
+
+for (let i =0 ; i <=10 ; i++) {
+    if (i === 10) {
+      break;
+    }
+    console.log(i);
+    i++;
+  }
+  console.log("Einde programma");
+
+     // Opdracht 9
+
+var i = 0;
+while (i < 20) {
+  if (i <= 10) {
+    console.log("Dit is loop: " + i);
+    console.log("Ik ontwikkel apps");
+  } else {
+    console.log("Dit is loop: " + i);
+  }
+  i++;
+}
+console.log("Einde programma");
